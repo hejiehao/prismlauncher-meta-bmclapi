3ecf33dd043760688d74cf518ff14aa66edd17ff
hejiehao <hejiehao12@126.com>
2024-06-01 23:58:15 +0800

    这神马答辩缩进

--- Sheep.py
+++ Sheep.py
@@ -18,19 +18,19 @@ if response.status_code == 200:
     current_date = datetime.now().strftime("%Y-%M-%d")
 
 
-# 构建要更新的数据使用量字符串
-data_usage_str = f"\n\n## 数据使用量更新 - {current_date}\n本月 GitHub Pages 消耗数据量: {monthly_data_usage} MB"
-
-# 读取README文件
-with open('README.md', 'r') as file:
-    lines = file.readlines()
-
-# 替换最后一行
-lines[-1] = data_usage_str + '\n\n'
-
-# 写入更新后的内容
-with open('README.md', 'w') as file:
-    file.writelines(lines)
+    # 构建要更新的数据使用量字符串
+    data_usage_str = f"\n\n## 数据使用量更新 - {current_date}\n本月 GitHub Pages 消耗数据量: {monthly_data_usage} MB"
+    
+    # 读取README文件
+    with open('README.md', 'r') as file:
+        lines = file.readlines()
+    
+    # 替换最后一行
+    lines[-1] = data_usage_str + '\n\n'
+    
+    # 写入更新后的内容
+    with open('README.md', 'w') as file:
+        file.writelines(lines)
 
 else:
     print("无法获取数据，请检查您的用户名、令牌或仓库名称。")
